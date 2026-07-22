7fc32bec3aadf969b95a562acd626132fd31f251
Oluchi Queen Iheagwara <oluchiqueeniheagwara@gmail.com>
2026-07-22 10:57:07 +0100

    Added State and Making the Buttons work

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
+import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 
 function App() {
   
-
   return (
     <main>
       <div id="game-container">
@@ -10,6 +10,7 @@ function App() {
           <Player initialName ="Player 1" symbol="X" />
           <Player initialName ="Player 2" symbol="O" />
         </ol>
+        <GameBoard />
       </div>
     </main>
     // <p>Coming Soon...</p>
